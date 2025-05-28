41cf36a3abc930d0c0c73a6d3da742cdba5a4b01
Shreya Patel <shreya.p@iqm.com>
2025-05-28 14:21:21 +0530

    unzipped zip folders

---
+++ script.js
@@ -0,0 +1,18 @@
+let navbar = document.querySelector('.header .navbar');
+
+window.onscroll=()=>{
+    navbar.classList.remove('active');
+    if(window.scrollY>0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+window.onload=()=>{
+    if(window.scrollY>0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
